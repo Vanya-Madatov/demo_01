4e55ac36236555eb8714f190ec741968bbc6e920
Vanya-Madatov <81025053+Vanya-Madatov@users.noreply.github.com>
2022-04-09 18:46:53 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -21,16 +21,28 @@ $('.menu-left-header-bars, .menu-block-shadow').click(function() {
     $('.menu-block').fadeOut(300);
 });
 
+$('.header-toggle-btn').click(function(){
+    $(this).closest('.header-toggle').find('.header-toggle-block').slideToggle(300);
+});
+jQuery(function($){
+  $(document).mouseup(function (e){ // событие клика по веб-документу
+    var div = $(".header-toggle-block"); // тут указываем ID элемента
+    if (!div.is(e.target) // если клик был не по нашему блоку
+        && div.has(e.target).length === 0) { // и не по его дочерним элементам
+      div.fadeOut(300); // скрываем его
+    }
+  });
+});
+
+
 
 $('.select-block-header .select-block-item').click(function(){
     $('.select-block-footer').not($(this).closest('.select-block').find('.select-block-footer')).slideUp(300)
     $(this).closest('.select-block').find('.select-block-footer').slideToggle(300);
 });
-
 $('.select-block-footer .select-block-item').on('click', function(){
   $(this).closest('.select-block').find('.select-block-footer').slideUp(300);
 });
 
-$('.header-toggle-btn').click(function(){
-    $(this).closest('.header-toggle').find('.header-toggle-block').slideToggle(300);
-});
+
+
